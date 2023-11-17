6d72e0b7465987b3295ec83a2aae8d8d92ff1e6e
Oaiker123 <tranbaoai2004@gmail.com>
2023-11-17 15:22:41 +0700

    first commit

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,11 @@
+<center><h3>• Website đồ án tốt nghiệp được chạy trên nền tảng host ảo localhost	<br/>
+  "Công ty CP Đầu tư Organica - Phát triển và phân phối thực phẩm hữu cơ cho người Việt" được thiết kế theo tiêu chi dễ 	<br/>
+ nhìn thân thiện với người dùng
+	<br/>
+• Được thực hiện bởi sinh viên:
+	<br/>
+○ Đặng Văn Hưng
+	<br/>
+○ Lê Văn Anh Dũng
+	<br/>
+• Trường Cao đẳng nghề Đà Nẵng</h3></center>
